7a12a1951ebeea94116ed86dd9367ed2b439034a
vivek kumar <vivek.kmr.mail@gmail.com>
2020-12-26 21:27:29 +0800

    Moved render tui env variable to main code

--- showcsv.go
+++ showcsv.go
@@ -1,9 +1,20 @@
 package showcsv
 
-import "errors"
+import (
+	"errors"
+	"os"
+)
 
 // Display function renders the tui table on the terminal
+// Please note that you have to set SHOWCSV_RENDER_TUI environment variable to render TUI.
+// `export SHOWCSV_RENDER_TUI=true`
+// You can save above line in your ~/.bashrc to make it persistent
 func Display(tableConfig *TableConfig) error {
+	_, renderTui := os.LookupEnv("SHOWCSV_RENDER_TUI")
+	if !renderTui {
+		return nil
+	}
+
 	app := newTuiApp(tableConfig)
 	if app == nil {
 		return errors.New("Error while creating tuiApp")
